fe04c32fe8443366996ba7c90ed9bbdfb9f5b8c1
Indiana Lora <indiana@ensaludybelleza.com>
2021-04-14 16:11:15 -0400

    less HTML more JAVASCRIPT

--- js/bounderies.js
+++ js/bounderies.js
@@ -1,30 +1,50 @@
 // Create bounderies page with the javascript DOM
 const body = document.body;
-const h1 = document.createElement("h2");
-h1.textContent = "Here i leave you my daily reminders";
-h1.id = "emp";
-body.append(h1);
+const h2 = document.createElement("h2");
+h2.textContent = "Here i leave you my daily reminders";
+h2.id = "emp";
+body.append(h2);
 
 const divVContainer = document.createElement("div");
 divVContainer.className = "vers-container";
 
 body.append(divVContainer);
 
-const divCards = document.createElement("div");
-divCards.className = "card";
-
-divVContainer.append(divCards);
-
-//how implement .map into the DOM
+//Loop through the portfolio array to add my daily bounderies
 const bounderies = [
-  { tittleName: "Emphaty", message: "" },
-  { tittleName: "Resilence", message: "" },
-  { tittleName: "Mindfullness", message: "" },
-  { tittleName: "Honesty", message: "" },
+  {
+    tittleName: "Emphaty",
+    message: "Empathy is about finding echoes of another person in yourself.",
+  },
+  {
+    tittleName: "Resilence",
+    message:
+      "“The oak fought the wind and was broken, the willow bent when it must and survived.”",
+  },
+  {
+    tittleName: "Mindfulness",
+    message: "You cannot control the results, only your actions.",
+  },
+  {
+    tittleName: "Honesty",
+    message:
+      "Integrity is telling myself the truth. And honesty is telling the truth to other people.",
+  },
+  {
+    tittleName: "Faith",
+    message:
+      "Faith is to believe what you do not see; the reward of this faith is to see what you believe.",
+  },
 ];
-
-const bounderyMap = bounderies.map((boundery) => {
-  return boundery.tittleName;
+//Analizar esto pls
+bounderies.map((boundery) => {
+  const divCards = document.createElement("div");
+  divCards.className = "card";
+  divVContainer.append(divCards);
+  const h5 = document.createElement("h5");
+  divCards.append(h5);
+  const divCardText = document.createElement("div");
+  divCards.append(divCardText);
+  divCardText.innerHTML = boundery.message;
+  return (h5.innerHTML = boundery.tittleName);
 });
-
-body.append(bounderyMap);
