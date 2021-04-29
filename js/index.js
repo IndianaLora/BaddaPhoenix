8bb6d312193b0e95e0fe52ad40f9d49698a51648
Indiana Lora <indiana@ensaludybelleza.com>
2021-04-29 09:45:31 -0400

    changed UI

---
+++ js/index.js
@@ -0,0 +1,59 @@
+var legendContainer = document.getElementById("skills-legend");
+
+var legendOne = document.createElement("div");
+legendOne.style.backgroundColor = "blue";
+legendOne.innerHTML="Hello"
+
+var legendOne = document.createElement("div");
+legendTwo.style.backgroundColor = "pink";
+
+var legendOne = document.createElement("div");
+legendThree.style.backgroundColor = "yellow";
+
+var legendOne = document.createElement("div");
+legendFour.style.backgroundColor = "purple";
+
+legendContainer.appendChild(legendTwo);
+
+// <!--
+//       <h1 id="separator" class="text-center">My projects</h1>
+//     </div>
+//     <div class="card-react">
+//       <h1>Language:</h1>
+//       <h2>C#</h2>
+//   </div>
+//   <div class="card-php1">
+//     <h1>Language:</h1>
+//     <h2>C#</h2>
+// </div>
+// <div class="card-c">
+//   <h1>Language:</h1>
+//   <h2>C#</h2>
+// </div>
+// <div class="card-jquery">
+//   <h1>Language:</h1>
+//   <h2>C#</h2>
+// </div>
+// <div class="card-php">
+//   <h1>Language:</h1>
+//   <h2>C#</h2>
+// </div>
+
+// <div class="project-card">
+//   <img src="./img/work.png"  />
+// </div>
+// </div>
+//     <div class="project-card">
+//         <img src="./img/work3.png" />
+
+//     </div>
+//     <div class="project-card">
+//         <img src="./img/work5.png" />
+
+//       </div>
+//       <div class="project-card">
+//         <img src="./img/work7.png"/>
+//       </div>
+//       <div class="project-card">
+//         <img src="./img/work1.png" />
+//       </div> -->
