bda1fff24866a79b62acad4b4711b416d998a664
Indiana Lora <indiana@ensaludybelleza.com>
2021-05-04 17:04:21 -0400

    pasantia.....VOY POR TI

--- js/proyect.js
+++ js/proyect.js
@@ -1,38 +1,38 @@
-// const proyectSection = document.getElementById("proyect-bolsa");
-// const proyectSection = document.getElementById("proyects-section");
-// const proyectSection = document.getElementById("proyects-section");
-// const proyectSection = document.getElementById("proyects-section");
-
+const proyectBolsa = document.getElementById("proyect-bolsa");
+const proyectAf= document.getElementById("proyect-Af");
+const proyectItla = document.getElementById("proyect-Itla");
+const proyectElecciones = document.getElementById("proyect-Elecciones");
+const proyectPokedex = document.getElementById("proyect-Pokedex");
 
 var img = document.createElement("img");
 img.src = "./img/bolsa.png";
 img.className = "img-proyects";
 
-proyectSection.append(img);
+proyectBolsa.append(img);
 
 
 var img1 = document.createElement("img");
 img1.src = "./img/afWolrd.jpeg";
 img1.className = "img-proyects";
 
-proyectSection.append(img1);
+proyectAf.append(img1);
 
 var img2 = document.createElement("img");
 img2.src = "./img/itla.png";
 img2.className = "img-proyects";
 
-proyectSection.append(img2);
+proyectItla.append(img2);
 
 var img3 = document.createElement("img");
 img3.src = "./img/Votacion.png";
 img3.className = "img-proyects";
 
-proyectSection.append(img3);
+proyectElecciones.append(img3);
 
 var img4 = document.createElement("img");
 img4.src = "./img/pokedex.png";
 img4.className = "img-proyects";
 
-proyectSection.append(img4);
+proyectPokedex.append(img4);
 
 
