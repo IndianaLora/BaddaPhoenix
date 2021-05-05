de2d24175c5e73f2e2a547ac6c1116d904aa07cc
Indiana Lora <indiana@ensaludybelleza.com>
2021-05-05 11:37:01 -0400

    im happy :D

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 const legendContainer = document.getElementById("skills-legend");
 
-
-
 //Created React legend
 var legendOne = document.createElement("div");
 legendOne.style.backgroundColor = "#4d0066";
 legendOne.id = "purple";
-legendOne.className = "legend-style";
+legendOne.classList = "legend-style";
 
 var react = document.createElement("h5");
 react.textContent = "Html";
-react.className="reverse"
+react.classList = "reverse";
 
 legendOne.append(react);
 legendContainer.append(legendOne);
@@ -19,11 +17,11 @@ legendContainer.append(legendOne);
 var legendTwo = document.createElement("div");
 legendTwo.style.backgroundColor = "#ff9900";
 legendTwo.id = "yellow";
-legendTwo.className = "legend-style";
+legendTwo.classList = "legend-style";
 
 var js = document.createElement("h5");
 js.textContent = "Css";
-js.className="reverse"
+js.classList = "reverse";
 
 legendTwo.append(js);
 legendContainer.append(legendTwo);
@@ -32,37 +30,41 @@ legendContainer.append(legendTwo);
 var legendThree = document.createElement("div");
 legendThree.style.backgroundColor = "#f56b6b";
 legendThree.id = "pink";
-legendThree.className = "legend-style";
+legendThree.classList = "legend-style";
 
 var css = document.createElement("h5");
 css.textContent = "Javascript";
-css.className="reverse"
+css.classList = "reverse";
 
 legendThree.append(css);
 legendContainer.append(legendThree);
 
 //Created Html legend
 var legendFour = document.createElement("div");
-legendFour.style.backgroundColor = "#00ccff";
+legendFour.style.backgroundColor = "#158aad";
 legendFour.id = "blue";
-legendFour.className = "legend-style";
+legendFour.classList = "legend-style";
 
 var html = document.createElement("h5");
 html.textContent = "React";
-html.className="reverse"
+html.classList = "reverse";
 
 legendFour.append(html);
 legendContainer.append(legendFour);
 
 //Created git legend
-var legend= document.createElement("div");
+var legend = document.createElement("div");
 legend.style.backgroundColor = "#e0111c";
 legend.id = "red";
-legend.className = "legend-style";
+legend.classList = "legend-style";
 
 var git = document.createElement("h5");
 git.textContent = "Git";
-git.className="reverse"
+git.classList = "reverse";
 
 legend.append(git);
 legendContainer.append(legend);
+
+const proyectHeader = document.getElementById("proyect-header");
+proyectHeader.innerHTML="Proyect i haved worked on..."
+proyectHeader.classList="proyect-header"
