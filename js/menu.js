e3d03999a75f42343268e8a3c94c43ea76474209
Indiana Lora <indiana@ensaludybelleza.com>
2021-06-18 11:58:02 -0400

    smoth for scroll and space for pages

--- js/menu.js
+++ js/menu.js
@@ -7,17 +7,15 @@ const projectsSection = document.getElementById("projectSection");
 const contact = document.getElementById("contact-a");
 const contactSection = document.getElementById("contactSection");
 
-function scrollMenu() {
-  if (home) {
-    homeSection.scrollIntoView();
-  }
-  if (skills) {
-    workSection.scrollIntoView();
-  }
-  if (projects) {
-    projectsSection.scrollIntoView();
-  }
-  if (contact) {
-    contactSection.scrollIntoView();
-  }
-}
+home.addEventListener("click", () => {
+  homeSection.scrollIntoView({ behavior: "smooth" });
+});
+skills.addEventListener("click", () => {
+  workSection.scrollIntoView({ behavior: "smooth" });
+});
+projects.addEventListener("click", () => {
+  projectsSection.scrollIntoView({ behavior: "smooth" });
+});
+contact.addEventListener("click", () => {
+  contactSection.scrollIntoView({ behavior: "smooth" });
+});
