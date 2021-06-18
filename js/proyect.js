667b56b9bc2803618d2b7cdfab543640aa643376
Indiana Lora <indiana@ensaludybelleza.com>
2021-06-18 12:53:32 -0400

    onClick event for projects

--- js/proyect.js
+++ js/proyect.js
@@ -9,27 +9,23 @@ var img = document.createElement("img");
 img.src = "./img/Bolsa.png";
 img.className = "img-proyects";
 proyectBolsa.append(img);
+img.addEventListener("click", () => {
+  window.open("https://github.com/IndianaLora/Bolsa-de-empleos#readme");
+});
 
 var img1 = document.createElement("img");
 img1.src = "./img/resin.png";
 img1.className = "img-proyects";
+img1.addEventListener("click", () => {
+  window.open("https://resin-webpage.herokuapp.com/");
+});
 
 proyectAf.append(img1);
 
 var img2 = document.createElement("img");
 img2.src = "./img/itla.png";
 img2.className = "img-proyects";
-
+img2.addEventListener("click", () => {
+  window.open("https://github.com/IndianaLora/Bolsa-de-empleos#readme");
+});
 proyectItla.append(img2);
-
-var img3 = document.createElement("img");
-img3.src = "./img/votacion.png";
-img3.className = "img-proyects";
-
-proyectElecciones.append(img3);
-
-var img4 = document.createElement("img");
-img4.src = "./img/pokedex.png";
-img4.className = "img-proyects";
-
-proyectPokedex.append(img4);
